52559a18d22f627871e0ac57ee638b0a3fb5d725
Anas Rchid <archid-@student.1337.ma>
2020-10-30 19:48:17 +0100

    move definition into graph.hpp

--- week3/include/graph.hpp
+++ week3/include/graph.hpp
@@ -12,10 +12,7 @@
 #include <utility>
 #include <vector>
 
-#define range(cont) std::begin(cont), std::end(cont)
-
-// FIXME: implement copy constructors
-// FIXME: implement assignment operators
+#define itr_range(cont) std::begin(cont), std::end(cont)
 
 // XXX: use smart pointers instead of raw pointers
 // XXX: use templates everywhere
@@ -23,7 +20,8 @@
 // TODO: reimplement public API
 
 namespace graph {
-template <typename T = int, typename W = T>
+using T = int;
+using W = T;
 class Graph {
   public:
     using vertex_id = std::size_t;
@@ -37,91 +35,14 @@ class Graph {
     class Vertex;
     class Edge;
 
-    using vertex_ptr = std::shared_ptr<class Vertex>;
-    using vertex_wptr = std::weak_ptr<class Vertex>;
+    using vertex_ref = std::reference_wrapper<Vertex>;
+    using vertex_ptr = std::shared_ptr<Vertex>;
+    using vertex_pref = const vertex_ptr &;
+    using vertex_wptr = std::weak_ptr<Vertex>;
 
-    using edge_ptr = std::shared_ptr<class Edge>;
-    using edge_wptr = std::weak_ptr<class Edge>;
-
-    class Vertex : std::enable_shared_from_this<Vertex> {
-        std::unordered_map<vertex_ptr, edge_ptr> _edges;
-        vertex_id _id;
-        vertex_value_t _val;
-        const vertex_ptr &self;
-
-      public:
-        Vertex() = delete;
-        Vertex(const Vertex &) = delete;
-        Vertex(Vertex &&other) = delete;
-
-        Vertex(vertex_id id, const vertex_value_t &val)
-            : _id(id), _val(val), self(this->sahred_from_this()) {}
-
-        Vertex &operator=(const Vertex &other) = delete;
-        Vertex &operator=(Vertex &&other) = delete;
-
-        vertex_id identity() const { return _id; }
-        const vertex_value_t &value() const { return _val; }
-        void value(const vertex_value_t &val) { _val = val; }
-
-        edge_wptr edge(vertex_wptr v) const {
-            return adjacent(v) ? _edges.at(v) : edge_wptr{};
-        }
-
-        bool adjacent(vertex_wptr v) const {
-            if (vertex_ptr ptr = v.lock(); ptr.expired())
-                throw std::runtime_error("Vertex was null");
-            else
-                return _edges.find(ptr) != std::end(_edges);
-        }
-
-        void add_directed_edge(vertex_wptr v, const edge_weight_t &wei) {
-            if (adjacent(v))
-                _edges.emplace(v, Edge::make_shared(self, v, wei));
-        }
-
-        void add_edge(vertex_ptr v, const edge_weight_t &wei) {
-            add_edge(v, wei, wei);
-        }
-
-        void add_edge(vertex_ptr v, const edge_weight_t &wei,
-                      const edge_weight_t &re_wei) {
-            add_directed_edge(v, wei);
-            v->add_directed_edge(self, re_wei);
-        }
-
-        void remove_directed_edge(vertex_wptr v) {
-            if (vertex_ptr ptr = v.lock(); ptr.expired())
-                throw std::runtime_error("Vertex was null");
-            _edges.erase(v);
-        }
-
-        void remove_edge(vertex_ptr v) {
-            remove_directed_edge(v);
-            v->remove_directed_edge(self);
-        }
-
-        std::vector<edge_wptr> edges() const {
-            std::vector<edge_wptr> vec;
-            vec.reserve(_edges.size());
-            std::transform(range(_edges), std::back_inserter(vec),
-                           [](const auto &pair) { return pair.second; });
-            return vec;
-        }
-
-        std::vector<vertex_wptr> neighbors() const {
-            std::vector<vertex_wptr> vec;
-            vec.reserve(_edges.size());
-            std::transform(std::begin(_edges), std::end(_edges),
-                           std::back_inserter(vec),
-                           [](const auto &pair) { return pair.first; });
-            return vec;
-        }
-
-        static vertex_ptr make_shared(const vertex_value_t &val) {
-            return std::make_shared(val);
-        }
-    };
+    using edge_ref = std::reference_wrapper<Edge>;
+    using edge_ptr = std::unique_ptr<Edge>;
+    using edge_pref = const edge_ptr &;
 
     class Edge {
         vertex_wptr _source, _sink;
@@ -132,7 +53,7 @@ class Graph {
         Edge(const Edge &) = delete;
         Edge(Edge &&other) = delete;
 
-        Edge(vertex_wptr from, vertex_wptr to, const edge_weight_t &wei)
+        Edge(vertex_pref from, vertex_pref to, const edge_weight_t &wei)
             : _source(from), _sink(to), _wei(wei) {
             if (not from or not to)
                 throw std::runtime_error("Vertex was null");
@@ -141,22 +62,91 @@ class Graph {
         Edge &operator=(const Edge &other) = delete;
         Edge &operator=(Edge &&other) = delete;
 
-        vertex_ptr from() const { return _source; }
-        vertex_ptr to() const { return _sink; }
-        const edge_weight_t &weight() const { return _sink; }
+        vertex_ptr from() const { return _source.lock(); }
+        vertex_ptr to() const { return _sink.lock(); }
+
+        const edge_weight_t &weight() const { return _wei; }
         void weight(const edge_weight_t &wei) { _wei = wei; }
 
-        // FIXME: implement residual edge
-        bool residual(const edge_wptr &other) const {}
-
-        static edge_ptr make_shared(vertex_wptr from, vertex_wptr to,
-                                    const edge_weight_t &wei) {
-            return std::make_shared(from, to, wei);
+        bool residual(const edge_ptr &other) const {
+            return from() == other->to() and to() == other->from();
         }
     };
 
-    vertex_t get_vertex(vertex_wptr v) const;
-    edge_t get_edge(edge_wptr e) const;
+    class Vertex : std::enable_shared_from_this<Vertex> {
+        using edge_map = std::unordered_map<vertex_id, edge_ptr>;
+
+        edge_map _edges;
+        vertex_id _id;
+        vertex_value_t _val;
+
+      public:
+        Vertex() = delete;
+        Vertex(const Vertex &) = delete;
+        Vertex(Vertex &&other) = delete;
+
+        Vertex(vertex_id id, const vertex_value_t &val) : _id(id), _val(val) {}
+
+        Vertex &operator=(const Vertex &other) = delete;
+        Vertex &operator=(Vertex &&other) = delete;
+
+        vertex_id identity() const { return _id; }
+        const vertex_value_t &value() const { return _val; }
+        void value(const vertex_value_t &val) { _val = val; }
+
+        const edge_ptr &edge(vertex_pref v) const { return _edges.at(v->_id); }
+
+        bool adjacent(vertex_pref v) const {
+            return _edges.find(v->_id) != std::end(_edges);
+        }
+
+        void add_directed_edge(vertex_pref v, const edge_weight_t &wei) {
+            if (adjacent(v))
+                return;
+            auto _edge = std::make_unique<Edge>(shared_from_this(), v, wei);
+            _edges.emplace(v->_id, std::move(_edge));
+        }
+
+        void add_edge(vertex_pref v, const edge_weight_t &wei) {
+            add_edge(v, wei, wei);
+        }
+
+        void add_edge(vertex_pref v, const edge_weight_t &wei,
+                      const edge_weight_t &re_wei) {
+            add_directed_edge(v, wei);
+            v->add_directed_edge(shared_from_this(), re_wei);
+        }
+
+        void remove_directed_edge(vertex_pref v) { _edges.erase(v->_id); }
+
+        void remove_edge(vertex_pref v) {
+            remove_directed_edge(v);
+            v->remove_directed_edge(shared_from_this());
+        }
+
+        std::vector<edge_ref> edges() const {
+            std::vector<edge_ref> vec;
+            const auto resolve = [](const edge_map::value_type &pair) {
+                return std::reference_wrapper(*pair.second);
+            };
+            vec.reserve(_edges.size());
+            std::transform(itr_range(_edges), std::back_inserter(vec), resolve);
+            return vec;
+        }
+
+        std::vector<vertex_ref> neighbors() const {
+            std::vector<vertex_ref> vec;
+            const auto resolve = [](const edge_map::value_type &pair) {
+                return std::reference_wrapper(*pair.second->to());
+            };
+            vec.reserve(_edges.size());
+            std::transform(itr_range(_edges), std::back_inserter(vec), resolve);
+            return vec;
+        }
+    };
+
+    vertex_t get_vertex(vertex_pref v) const;
+    edge_t get_edge(const edge_ptr &e) const;
     std::pair<vertex_id, vertex_id> generate_edge(std::size_t limit);
 
   public:
@@ -204,47 +194,46 @@ class Graph {
     void vertex_check(bool in, vertex_id id, const Args &... msg) const;
 };
 
-// FIXME: implement operator<<
+// namespace impl {
+// template <typename T, typename W>
+// using Vertex = typename Graph<T, W>::Vertex;
 
-namespace impl {
-template <typename T, typename W>
-using Vertex = typename Graph<T, W>::Vertex;
+// template <typename T, typename W>
+// using Edge = typename Graph<T, W>::Edge;
 
-template <typename T, typename W>
-using Edge = typename Graph<T, W>::Edge;
+// template <typename T, typename W>
+// using vertex_id = typename Graph<T, W>::vertex_id;
 
-template <typename T, typename W>
-using vertex_id = typename Graph<T, W>::vertex_id;
+// template <typename T, typename W = void>
+// using vertex_value_t = typename Graph<T, W>::vertex_value_t;
 
-template <typename T, typename W = void>
-using vertex_value_t = typename Graph<T, W>::vertex_value_t;
+// template <typename T, typename W>
+// using vertex_t = typename Graph<T, W>::vertex_t;
 
-template <typename T, typename W>
-using vertex_t = typename Graph<T, W>::vertex_t;
+// template <typename T, typename W>
+// using edge_weight_t = typename Graph<T, W>::edge_weight_t;
 
-template <typename T, typename W>
-using edge_weight_t = typename Graph<T, W>::edge_weight_t;
+// template <typename T, typename W>
+// using edge_t = typename Graph<T, W>::edge_t;
 
-template <typename T, typename W>
-using edge_t = typename Graph<T, W>::edge_t;
+// template <typename T, typename W>
+// using vertex_ptr = typename Graph<T, W>::vertex_ptr;
 
-template <typename T, typename W>
-using vertex_ptr = typename Graph<T, W>::vertex_ptr;
+// template <typename T, typename W>
+// using vertex_wptr = typename Graph<T, W>::vertex_wptr;
 
-template <typename T, typename W>
-using vertex_wptr = typename Graph<T, W>::vertex_wptr;
+// template <typename T, typename W>
+// using vertex_constref = const vertex_ptr<T, W> &;
 
-template <typename T, typename W>
-using vertex_constref = const vertex_ptr<T, W> &;
+// template <typename T, typename W>
+// using edge_ptr = typename Graph<T, W>::edge_ptr;
 
-template <typename T, typename W>
-using edge_ptr = typename Graph<T, W>::edge_ptr;
+// template <typename T, typename W>
+// using edge_wptr = typename Graph<T, W>::edge_wptr;
 
-template <typename T, typename W>
-using edge_wptr = typename Graph<T, W>::edge_wptr;
-
-template <typename T, typename W>
-using edge_constref = const edge_ptr<T, W> &;
-} // namespace impl
+// template <typename T, typename W>
+// using edge_constref = const edge_ptr<T, W> &;
+// }
+// namespace impl
 } // namespace graph
 #endif /* GRAPH_H */
