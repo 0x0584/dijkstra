336cca43a112e888d2404acdb3468ff445d9d7a5
Anas Rchid <archid-@student.1337.ma>
2020-11-02 14:41:57 +0100

    remove complete graph example

--- week3/src/main.cpp
+++ week3/src/main.cpp
@@ -1,5 +1,3 @@
-#include <iomanip>
-
 #include "graph.hpp"
 
 std::size_t n_verts = 0, n_edges = 0;
@@ -24,26 +22,15 @@ int main(int, char const *[]) {
                   << " edges\n";
     }
     {
-        Graph k;
-        for (std::size_t i = 0; i < n_verts; ++i) {
-            for (std::size_t j = 0; j < n_verts; ++j) {
-                if (i == j)
-                    continue;
-                if (not k.has_vertex(i))
-                    k.add_vertex(i, 1);
-                if (not k.has_vertex(j))
-                    k.add_vertex(j, 1);
-                k.add_directed_edge(i, j, 1);
-            }
-        }
-        std::cout << "complete graph has " << k.edges().size() << " edges\n";
+        std::size_t k = (n_verts * (n_verts - 1));
+        std::cout << "complete graph has " << k << " edges\n";
         std::cout << std::endl;
     }
 
     for (double d = 0; d <= 1.05; d += 0.05) {
         Graph h{n_verts, std::min(d, 1.0)};
-        std::cout << "edge density: " << std::setprecision(3) << d << " has "
-                  << h.edges().size() << " edges\n";
+        std::cout << "edge density: " << d << " has " << h.edges().size()
+                  << " edges\n";
     }
     return 0;
 }
